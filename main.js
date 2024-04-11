8f65b9d96e54692abf2ae189898abf838523f056
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2024-04-11 16:50:26 -0300

    1

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const dataatual= new Date();
 let dataNiver= prompt ("data proximo niver:");
 dataNiver= mew Date (dataNiver+"T23:59:59");
 let diasQueFaltam= math.foor(dataNiver-dataatual)/86400000
-document.querySelector("#dias_restantes"). textContent= diasQueFaltam;
+document.querySelector("#dias_restantes").textContent= diasQueFaltam;
